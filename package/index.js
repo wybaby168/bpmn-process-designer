37aeb544fd5c9a827e0a7887554b91cca186380c
wangyu <727842003@qq.com>
2023-02-08 11:58:58 +0800

    feat: 优化依赖

--- package/index.js
+++ package/index.js
@@ -2,6 +2,12 @@ import MyProcessDesigner from "./designer";
 import MyProcessPalette from "./palette";
 import MyProcessPenal from "./penal";
 
+// 自定义元素选中时的弹出菜单（修改 默认任务 为 用户任务）
+import CustomContentPadProvider from "../package/designer/plugins/content-pad";
+// 自定义左侧菜单（修改 默认任务 为 用户任务）
+import CustomPaletteProvider from "../package/designer/plugins/palette";
+import Log from "../package/Log";
+
 const components = [MyProcessDesigner, MyProcessPenal, MyProcessPalette];
 
 const install = function(Vue) {
@@ -19,3 +25,5 @@ export default {
   install,
   ...components
 };
+
+export { CustomContentPadProvider, CustomPaletteProvider, Log };
